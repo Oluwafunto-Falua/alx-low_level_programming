58381489a2dc5e241c58b9c762e245e9d54662be
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-04 08:31:02 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {	
-printf("%d is negative\n" ,n);	
+printf("%d is negative\n", n);	
 }
 else if (n == 0)
 {
-printf("% is zero\n" , n);
+printf("% is zero\n", n);
 }
 else
 {
-printf("% is positive\n" , n);
+printf("% is positive\n", n);
 }
 return (0)
 }
