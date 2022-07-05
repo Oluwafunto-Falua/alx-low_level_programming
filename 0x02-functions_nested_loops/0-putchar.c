f1413a079bf6c1044b038ab1afaf3ec060fbcde1
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-05 15:03:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,17 @@
-#include <main .h>
-/** Write a program that prints _putchar, followed by a new line
+#include "main.h"
+/**
+* main - function to print
+* Return: 0
 */
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+putchar('');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');									return (0);
 }
