89b2cfd5854d0df2ca202d2984ad2059b366849c
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-04 18:28:43 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-* main - prints all numbers of base 16.
-/**
- * main - Prints all the numbers of base 16 in lowercase.
- * Return: Always 0.
- */
+* main - Prints all the numbers of base 16 in lowercase.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 int num;
