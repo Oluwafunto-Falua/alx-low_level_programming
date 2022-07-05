5a286e1937dafdcb32e762613dc3fdba2188b664
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-04 17:58:41 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
-* main - prints the alphabet in reverse.
-(*
-* Return: 0 on success
-*/
+* main - prints all numbers of base 16.
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ * Return: Always 0.
+ */
 int main(void)
 {
-char c = 'z';
-while (c >= 'a')
-{
-putchar(c);
-c--;
-}
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
