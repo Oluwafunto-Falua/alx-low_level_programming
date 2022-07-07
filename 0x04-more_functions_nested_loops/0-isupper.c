8ffd99af4b99e25e0e327847373c7e6603b1abb1
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-07 14:02:56 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
 /**
-*_isupper - a function that checks for uppercase character
-* @c: input
-* Return: 1 if c is uppercase, 0 otherwise
+* _isdigit - checks if parameter is a number between 0 to 9.
+*@c: input number.
+*Return: 1 if is a number (0 to 9), 0 in other case.
 */
-int _isupper(int c)
+int _isdigit(int c)
 {
-return (c >= 'A' && c <= 'Z');
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
