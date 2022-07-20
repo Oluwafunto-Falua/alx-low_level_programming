336d066179cb51b7bdeb8dbfd3cc1191fdeec978
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-20 15:17:46 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-*
-*  * _strlen_recursion - size
-*
-*   * @s: pointer to string params
-*
-*    * Return: recursion
-*
-*/
+ *  * _strlen_recursion - size
+ *
+ *   * @s: pointer to string params
+ *
+ *    * Return: recursion
+ *
+ */
 int _strlen_recursion(char *s)
 {
 if (!*s)
