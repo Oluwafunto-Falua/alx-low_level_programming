0b7bfffb0ec5a4856ed1866239a871b08c4abb5f
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-07-22 14:38:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - program that prints its name
+* @argc: counts nuber of arguments
+* @argv: the program name
+* Return: zero is success
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
