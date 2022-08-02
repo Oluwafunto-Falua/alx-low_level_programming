c7994a4e4ce3129277573716d787efa06b06de36
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-08-02 13:20:48 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
-#include < stdio.h >
+#include <stdio.h>
 /**
-* main - Print the name of the file compiled from
-* Return: result
+* main - prints the name of the file.
+*    * Return: Always 0.
 */
 int main(void)
 {
