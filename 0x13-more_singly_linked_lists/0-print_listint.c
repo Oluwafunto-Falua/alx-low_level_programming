218e48843acb5e9fa4a55c1e4a9d780cbfedf41d
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-08-16 14:06:27 -0700

    0-main.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_listint - prints linked list
+* @h:linked list
+* Return: number of nodes.
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
