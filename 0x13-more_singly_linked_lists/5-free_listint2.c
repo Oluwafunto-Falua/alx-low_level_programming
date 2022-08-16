9dc0a7ace1b97356195f8ac9972dd5554f615024
Oluwafunto-Falua <Oluwafunto.Falua@metricinternet.com>
2022-08-16 14:54:15 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* free_listint2 - frees a list
+* @head: list
+* Return: void
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *mem_loc;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+mem_loc = (*head)->next;
+free(*head);
+*head = mem_loc;
+}
+}
